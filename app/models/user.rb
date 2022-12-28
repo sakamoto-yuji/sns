239ead535101bfac9751a353c7a5984969f3fe23
sakamoto-yuji <chaser.yuji.23@gmail.com>
2022-12-28 15:42:28 +0900

    ログイン機能途中まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
  validates :email,{presence:true}
  validates :email,{uniqueness:true}
+
+ validates :password,{presence:true}
+ valid_password = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ validates :password, format: { with:valid_password}
 end
