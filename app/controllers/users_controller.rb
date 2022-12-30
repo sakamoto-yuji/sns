6e354eb1d0c511831dbe34dbbff24bbd9ce4fca0
sakamoto-yuji <chaser.yuji.23@gmail.com>
2022-12-30 09:04:15 +0900

    ログイン機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user,{only:[:index,:show,:edit,:update]}
+  before_action :forbid_login_user,{only:[:new,:create,:login_form,:login]}
+  before_action :ensure_correct_user,{only:[:edit,:update,:destroy]}
+
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+
   def index
     @users = User.all.order(created_at: :desc)
   end
@@ -19,11 +30,12 @@ class UsersController < ApplicationController
       password: params[:password]
       ) 
     if @user.save
+      session[:user_id] = @user.id
 
       if @user.image_name.present?
-        image = params[:image]
-        File.binwrite("public/user_images/#{@user.id}.jpg",image.read)
         @user.image_name = "#{@user.id}.jpg"
+        image = params[:image]
+        File.binwrite("public/user_images/#{@user.image_name}",image.read)
         @user.save
       else
         @user.image_name = "default_image.jpg"
@@ -78,10 +90,31 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email],
                          password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+
+      if User.find_by(email: params[:email])
+      else
+      @wrong_email = "メールアドレスが間違っています"
+      end
+
+      if User.find_by(password: params[:password])
+      else
+      @wrong_password = "パスワードが間違っています"
+      end
+
+      @email = params[:email]
+      @password = params[:password]
       render("users/login_form")
     end
+  end
+
+  def logout
+      session[:user_id] = nil
+      flash[:notice] = "ログアウトしました"
+      redirect_to("/login")
   end
 end
