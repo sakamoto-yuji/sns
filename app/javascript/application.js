a265d5f6cb9238e9a2dc7d1751def9129833cc51
sakamoto-yuji <chaser.yuji.23@gmail.com>
2023-01-22 06:40:55 +0900

    いいね機能、ルーティング改修、bootstrap導入、スタイル修正、、、

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// require jquery
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery-rails"
+
+// フラッシュメッセージ
+$(function(){
+  $('.flash').fadeOut(4000);  //４秒かけて消えていく
+});
+
 
