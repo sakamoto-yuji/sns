bd17a17847d6ac84e805b71ae4eacc9972ad5447
sakamoto-yuji <chaser.yuji.23@gmail.com>
2023-01-26 05:26:27 +0900

    ログイン機能の欠陥を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
  validates :email,{presence:true , uniqueness:true}
 
  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
- validates :password, presence:true,
+ validates :password, { presence:true, allow_nil:true,
                       length: { minimum: 8, allow_blank:true }, 
-                      format: { with: VALID_PASSWORD_REGEX}, allow_blank:true,
-                      allow_nil:true
+                      format: { with: VALID_PASSWORD_REGEX}, allow_blank:true  }
 
  has_secure_password
  has_many :posts, dependent: :destroy
